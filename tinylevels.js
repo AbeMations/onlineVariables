7cefdc4b8cf4315eae4e3e9e1d805a1f3a28eadc
AbeMations <39106034+AbeMations@users.noreply.github.com>
2018-11-04 00:32:13 -0400

    Update tinylevels.js

--- tinylevels.js
+++ tinylevels.js
@@ -1,15 +1,1 @@
-var width;
-var height;
-
-function setup() {
-  createCanvas(window.innerWidth, window.innerHeight);
-  width = window.innerWidth;
-  height = window.innerHeight;
-  noStroke();
-}
-
-function draw() {
-  background(0);
-  fill(255);
-  rect(mouseX, mouseY, 50, 50);
-}
+console.log("Hello world!");
